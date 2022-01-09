4087a2a4219d87fff56db633c0b2411d522b2a0c
zhouguoming <1060968795@qq.com>
2022-01-09 10:39:41 +0800

    初始化项目添加依赖包、application.yml

---
+++ emp_admin/src/main/java/com/zgm/server/EmpAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zgm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpAdminApplication.class, args);
+    }
+
+}
---
+++ emp_admin/src/test/java/com/zgm/server/EmpAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
